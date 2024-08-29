8d35f34dd4ccb376176aea20302cb9a3eb27d791
Liudvikas Taluntis <43747286+Duslerke@users.noreply.github.com>
2024-08-29 16:11:58 +0100

    Remove the AWS RDS postgres instance module resource.

--- terraform/production/main.tf
+++ terraform/production/main.tf
@@ -51,24 +51,3 @@ data "aws_ssm_parameter" "electoral_register_postgres_db_password" {
 data "aws_ssm_parameter" "electoral_register_postgres_username" {
   name = "/electoral-register-api/production/postgres-username"
 }
-
-module "postgres_db_production" {
-  source               = "github.com/LBHackney-IT/aws-hackney-common-terraform.git//modules/database/postgres"
-  environment_name     = "production"
-  vpc_id               = data.aws_vpc.production_vpc.id
-  db_identifier        = "electoral-register-mirror-db"
-  db_name              = "electoral_register_mirror"
-  db_port              = 5400
-  subnet_ids           = data.aws_subnet_ids.production.ids
-  db_engine            = "postgres"
-  db_engine_version    = "11.1"
-  db_instance_class    = "db.t2.micro"
-  db_allocated_storage = 20
-  maintenance_window   = "sun:10:00-sun:10:30"
-  db_username          = data.aws_ssm_parameter.electoral_register_postgres_username.value
-  db_password          = data.aws_ssm_parameter.electoral_register_postgres_db_password.value
-  storage_encrypted    = false
-  multi_az             = true //only true if production deployment
-  publicly_accessible  = false
-  project_name         = "platform apis"
-}
